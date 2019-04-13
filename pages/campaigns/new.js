af297ca8437459e8ba3a3c37da9a8c315c86bed8
Spencer <swhite215@live.com>
2019-04-13 12:46:46 -0400

    added nested new campaign component route

---
+++ pages/campaigns/new.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class CampaignNew extends Component {
+    render() {
+        return (
+            <div>
+                <h1>New Campaign</h1>
+            </div>
+        );
+    }
+}
+
+export default CampaignNew;
