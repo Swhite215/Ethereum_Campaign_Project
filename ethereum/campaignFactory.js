d29cc35f48d907d22948d0e78ac4f49bcbf51aba
Spencer <swhite215@live.com>
2019-04-12 20:07:19 -0400

    connection to deployed factory instance

---
+++ ethereum/campaignFactory.js
@@ -0,0 +1,11 @@
+import web3 from "./web3";
+import compiledFactory from "./build/CampaignFactory";
+
+const address = "0x7C7B0C41C4B3166c6eCE4F16239b17526bB79Dd0";
+
+const instance = new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface),
+    address
+);
+
+export default instance;
