37739665b292920fc4b7f8c8ea9f450f41ab8be6
Spencer <swhite215@live.com>
2019-04-12 20:40:41 -0400

    root component with componentDIdMount

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,37 @@
 import React from "react";
+import campaignFactory from "../ethereum/campaignFactory";
+import web3 from "../ethereum/web3";
 
-export default () => {
-    return <h1>This is the root page!</h1>;
-};
+class CampaignIndex extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            message: "Hello"
+        };
+    }
+
+    async componentDidMount() {
+        const campaigns = await campaignFactory.methods
+            .getDeployedCampaigns()
+            .call();
+
+        console.log(campaigns);
+    }
+
+    render() {
+        // window.addEventListener("load", async () => {
+        //     if (window.ethereum) {
+        //         try {
+        //             await window.ethereum.enable();
+        //         } catch (e) {
+        //             //User denied account access
+        //         }
+        //     }
+        // });
+
+        return <h1>This is the root app!</h1>;
+    }
+}
+
+export default CampaignIndex;
