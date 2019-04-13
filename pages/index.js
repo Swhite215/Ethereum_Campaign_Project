a1bc91bb3094e31af1e8d0a7c2c5a3250edfa027
Spencer <swhite215@live.com>
2019-04-13 11:21:13 -0400

    added card rendering for campaign contracts

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import campaignFactory from "../ethereum/campaignFactory";
 import web3 from "../ethereum/web3";
+import { Card } from "semantic-ui-react";
 
 class CampaignIndex extends React.Component {
     constructor(props) {
@@ -20,6 +21,18 @@ class CampaignIndex extends React.Component {
         return { campaigns };
     }
 
+    renderCampaigns() {
+        const items = this.props.campaigns.map(campaign => {
+            return {
+                header: campaign,
+                description: <a>View Campaign</a>,
+                fluid: true
+            };
+        });
+
+        return <Card.Group items={items} />;
+    }
+
     render() {
         // window.addEventListener("load", async () => {
         //     if (window.ethereum) {
@@ -31,7 +44,15 @@ class CampaignIndex extends React.Component {
         //     }
         // });
 
-        return <div>{this.props.campaigns[0]}</div>;
+        return (
+            <div>
+                <link
+                    rel="stylesheet"
+                    href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
+                />
+                {this.renderCampaigns()}
+            </div>
+        );
     }
 }
 
