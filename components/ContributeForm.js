24fa9276415b83751a53bae979ada23a6f38e798
Spencer <swhite215@live.com>
2019-04-13 19:10:16 -0400

    added code for contributing to campaign and aux

--- components/ContributeForm.js
+++ components/ContributeForm.js
@@ -1,22 +1,79 @@
 import React, { Component } from "react";
 import { Form, Input, Message, Button } from "semantic-ui-react";
+import campaignInstance from "../ethereum/campaign";
+import web3 from "../ethereum/web3";
+import { Router } from "../routes";
 
 class ContributeForm extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            loading: false
+            loading: false,
+            value: "",
+            errorMessage: ""
         };
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
     }
+
+    onChange(e) {
+        this.setState({ value: e.target.value });
+    }
+
+    async onSubmit(e) {
+        e.preventDefault();
+
+        this.setState({
+            loading: true,
+            errorMessage: ""
+        });
+
+        let campaign = campaignInstance(this.props.address);
+
+        try {
+            let accounts = await web3.eth.getAccounts();
+
+            await campaign.methods.contribute().send({
+                from: accounts[0],
+                value: web3.utils.toWei(this.state.value, "ether")
+            });
+
+            Router.replaceRoute(`/campaigns/${this.props.address}`);
+        } catch (e) {
+            this.setState({
+                errorMessage: e.message
+            });
+        }
+
+        this.setState({
+            loading: false,
+            value: ""
+        });
+    }
+
     render() {
         return (
-            <Form>
+            <Form error={!!this.state.errorMessage} onSubmit={this.onSubmit}>
                 <Form.Field>
                     <label>Amount to Contribute</label>
-                    <Input label="ether" labelPosition="right" />
+                    <Input
+                        value={this.state.value}
+                        label="ether"
+                        labelPosition="right"
+                        onChange={this.onChange}
+                    />
                 </Form.Field>
-                <Button primary>Contribute!</Button>
+                <Button primary loading={this.state.loading}>
+                    Contribute!
+                </Button>
+
+                <Message
+                    error
+                    header="Oops!"
+                    content={this.state.errorMessage}
+                />
             </Form>
         );
     }
