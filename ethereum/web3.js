027742ee72de8b5b7414a25446e03fbb67d56b2a
Spencer <swhite215@live.com>
2019-04-12 20:59:53 -0400

    browser or server web3 instantiation

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,12 +1,19 @@
 import Web3 from "web3";
 
-//Create instance of Web3 that uses MetaMask provider
+//Create instance of Web3 that uses MetaMask provide
 let web3;
 
-if (window.ethereum) {
+//Check we are in the browser and MetaMask is running
+if (typeof window !== "undefined" && window.ethereum) {
+    //Browser Code
     web3 = new Web3(window.ethereum);
 } else {
-    web3 = new Web3(window.web3.currentProvider);
+    //Server Code, set up our own provider using Infura
+    const provider = new Web3.providers.HttpProvider(
+        "https://rinkeby.infura.io/v3/34feb9723d5448ed98ce1abc279d197a"
+    );
+
+    web3 = new Web3(provider);
 }
 
 export default web3;
