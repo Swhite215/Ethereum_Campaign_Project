66f973ce8fffd6487f91503acc55ed1a75bdd448
Spencer <swhite215@live.com>
2019-04-13 13:29:04 -0400

    added submit handler to create campaign contract

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,16 +1,20 @@
 import React, { Component } from "react";
 import Layout from "../../components/Layout";
-import { Form, Button, Input } from "semantic-ui-react";
+import { Form, Button, Input, Message } from "semantic-ui-react";
+import factoryContract from "../../ethereum/campaignFactory";
+import web3 from "../../ethereum/web3";
 
 export class CampaignNew extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            minimumContribution: ""
+            minimumContribution: "",
+            errorMessage: ""
         };
 
         this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
     }
 
     onChange(e) {
@@ -19,11 +23,32 @@ export class CampaignNew extends Component {
         });
     }
 
+    async onSubmit(e) {
+        e.preventDefault();
+
+        try {
+            await window.ethereum.enable();
+
+            const accounts = await web3.eth.getAccounts();
+
+            console.log(accounts);
+
+            await factoryContract.methods
+                .createCampaign(this.state.minimumContribution)
+                .send({ from: accounts[0] });
+        } catch (e) {
+            console.log(e);
+            this.setState({
+                errorMessage: e.message
+            });
+        }
+    }
+
     render() {
         return (
             <Layout>
                 <h3>New Campaign</h3>
-                <Form>
+                <Form onSubmit={this.onSubmit}>
                     <Form.Field>
                         <label>Minimum Contribution</label>
                         <Input
