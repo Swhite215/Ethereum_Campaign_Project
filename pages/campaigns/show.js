23377042c9dec825acab2589eb12188c6e49ea2e
Spencer <swhite215@live.com>
2019-04-13 15:54:21 -0400

    initial props method to get contract details

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,7 +1,22 @@
 import React, { Component } from "react";
 import Layout from "../../components/Layout";
+import campaignInstance from "../../ethereum/campaign";
 
 class CampaignShow extends Component {
+    static async getInitialProps(props) {
+        let campaign = campaignInstance(props.query.address);
+        let summary = await campaign.methods.getSummary().call();
+
+        //Translate summary values to usable props
+        return {
+            minimumContribution: summary[0],
+            balance: summary[1],
+            requestsCount: summary[2],
+            approversCount: summary[3],
+            manager: summary[4]
+        };
+    }
+
     render() {
         return (
             <Layout>
