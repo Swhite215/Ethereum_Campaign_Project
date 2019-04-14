342fedf9ed3ac6bfb317c3ee28ed26f3343e12eb
Spencer <swhite215@live.com>
2019-04-13 20:11:31 -0400

    code for grabbing all campaign requests

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -2,13 +2,36 @@ import React, { Component } from "react";
 import Layout from "../../../components/Layout";
 import { Button } from "semantic-ui-react";
 import { Link } from "../../../routes";
+import campaignInstance from "../../../ethereum/campaign";
+import web3 from "../../../ethereum/web3";
 
 class RequestIndex extends Component {
     static async getInitialProps(props) {
         const { address } = props.query;
-        return {
-            address
-        };
+
+        try {
+            let accounts = await web3.eth.getAccounts();
+
+            let campaign = campaignInstance(address);
+
+            let requestCount = await campaign.methods.getRequestsCount().call();
+
+            const requests = await Promise.all(
+                Array(parseInt(requestCount))
+                    .fill()
+                    .map((element, index) => {
+                        return campaign.methods.requests(index).call();
+                    })
+            );
+
+            return {
+                address,
+                requests,
+                requestCount
+            };
+        } catch (e) {
+            console.log(e);
+        }
     }
 
     render() {
