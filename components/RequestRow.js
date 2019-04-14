bd61b2a1bebcd753b9dab2dbf8eda634f431da21
Spencer <swhite215@live.com>
2019-04-13 21:01:55 -0400

    added onApprove and onFinalize methods

--- components/RequestRow.js
+++ components/RequestRow.js
@@ -2,10 +2,55 @@ import React, { Component } from "react";
 import { Table, Button } from "semantic-ui-react";
 import web3 from "../ethereum/web3";
 import campaignInstance from "../ethereum/campaign";
+import { Router } from "../routes";
 
 class RequestRow extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            approveLoading: false
+        };
+
+        this.onApprove = this.onApprove.bind(this);
+        this.onFinalize = this.onFinalize.bind(this);
+    }
+
+    async onApprove() {
+        try {
+            this.setState({ approveLoading: true });
+            const accounts = await web3.eth.getAccounts();
+
+            let campaign = campaignInstance(this.props.address);
+
+            await campaign.methods
+                .approveRequest(this.props.id)
+                .send({ from: accounts[0] });
+        } catch (e) {
+            console.log(e);
+        }
+
+        this.setState({ approveLoading: false });
+
+        Router.replaceRoute(`/campaigns/${this.props.address}/requests`);
+    }
+
+    async onFinalize() {
+        try {
+            this.setState({ finalizeLoading: true });
+            const accounts = await web3.eth.getAccounts();
+
+            let campaign = campaignInstance(this.props.address);
+
+            await campaign.methods
+                .finalizeRequest(this.props.id)
+                .send({ from: accounts[0] });
+        } catch (e) {
+            console.log(e);
+        }
+
+        this.setState({ finalizeLoading: false });
+
+        Router.replaceRoute(`/campaigns/${this.props.address}/requests`);
     }
 
     render() {
@@ -19,10 +64,11 @@ class RequestRow extends Component {
             approvalCount
         } = this.props.request;
         const { approversCount } = this.props;
+        const readyToFinalize = approvalCount > approversCount / 2;
 
         return (
             <React.Fragment>
-                <Row>
+                <Row disabled={complete} positive={readyToFinalize}>
                     <Cell>{id}</Cell>
                     <Cell>{description}</Cell>
                     <Cell>{web3.utils.fromWei(value, "ether")}</Cell>
@@ -31,12 +77,28 @@ class RequestRow extends Component {
                         {approvalCount}/{approversCount}
                     </Cell>
                     <Cell>
-                        <Button color="green" basic>
-                            Approve
-                        </Button>
+                        {complete ? null : (
+                            <Button
+                                color="green"
+                                basic
+                                loading={this.state.approveLoading}
+                                onClick={this.onApprove}
+                            >
+                                Approve
+                            </Button>
+                        )}
                     </Cell>
                     <Cell>
-                        <Button>Finalize</Button>
+                        {complete ? null : (
+                            <Button
+                                color="teal"
+                                basic
+                                loading={this.state.finalizeLoading}
+                                onClick={this.onFinalize}
+                            >
+                                Finalize
+                            </Button>
+                        )}
                     </Cell>
                 </Row>
             </React.Fragment>
