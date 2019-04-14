57cac4e469776bfedf0f41802a76bb63a61a20c9
Spencer <swhite215@live.com>
2019-04-13 20:15:55 -0400

    added table header code

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Layout from "../../../components/Layout";
-import { Button } from "semantic-ui-react";
+import { Button, Table } from "semantic-ui-react";
 import { Link } from "../../../routes";
 import campaignInstance from "../../../ethereum/campaign";
 import web3 from "../../../ethereum/web3";
@@ -35,6 +35,7 @@ class RequestIndex extends Component {
     }
 
     render() {
+        const { Header, Row, HeaderCell, Body } = Table;
         return (
             <Layout>
                 <h3>Request List</h3>
@@ -43,6 +44,18 @@ class RequestIndex extends Component {
                         <Button primary>Add Request</Button>
                     </a>
                 </Link>
+                <Table>
+                    <Header>
+                        <Row>
+                            <HeaderCell>ID</HeaderCell>
+                            <HeaderCell>Description</HeaderCell>
+                            <HeaderCell>Amount</HeaderCell>
+                            <HeaderCell>Recipient</HeaderCell>
+                            <HeaderCell>Approve</HeaderCell>
+                            <HeaderCell>Finalize</HeaderCell>
+                        </Row>
+                    </Header>
+                </Table>
             </Layout>
         );
     }
